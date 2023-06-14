ebc2ceebb9433eb174022765b0846854e61aa66b
naveenkaviyarasan <131254634+naveenkaviyarasan@users.noreply.github.com>
2023-06-14 09:56:45 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,15 @@
+class Assignment{
+public static void main(String args[]){
+int a = 10;
+int b = 20;
+//System.out.println("a value is " + a);
+//System.out.println("b value is " + b);
+a+=5;
+System.out.println(a);
+b+=20;
+System.out.println(b);
+
+
+
+}
+}
---
+++ LogicalBitwise.java
@@ -0,0 +1,21 @@
+class LogicalBitwise{
+public static void main(String args[]){
+int a = 3;
+int b = 4;
+System.out.println(a<10 && a<12);
+System.out.println(a>10 || a<12);
+System.out.println(!(a>10 && a<12));
+System.out.println(a<b);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a&b);
+System.out.println(a|b);
+System.out.println(a^b);
+System.out.println(~a);
+System.out.println(~b);
+
+
+
+
+}
+}

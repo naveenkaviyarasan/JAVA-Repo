e8cb8a4ca9817dfad39fb4ca9c77a625601abfb1
naveenkaviyarasan <131254634+naveenkaviyarasan@users.noreply.github.com>
2023-06-15 18:37:46 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Swap {
+public static void main(String args[]){
+int a;
+int b;
+System.out.println("Enter the A value");
+Scanner sc = new Scanner(System.in);
+int A = sc.nextInt();
+System.out.println("Enter the B value");
+//Scanner sc = new Scanner(System.in);
+int B = sc.nextInt();
+
+A = A^B;
+B = A^B;
+A = A^B;
+
+     System.out.println("After swapping:");
+        System.out.println("a = " + A);
+        System.out.println("b = " + B);
+    }
+}
+
+
---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+class TypeCasting{
+public static void main(String args[]){
+//int a = 10;
+//double b = a;
+double a = 22.123d;
+int b = (int)a;
+System.out.println(a);
+System.out.println(b);
+
+}
+}

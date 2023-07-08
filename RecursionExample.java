c7da17dc4fa62e53c9f4d84dc9c9fcd211380507
naveenkaviyarasan <131254634+naveenkaviyarasan@users.noreply.github.com>
2023-07-08 13:10:39 +0530

    add Java

---
+++ RecursionExample.java
@@ -0,0 +1,27 @@
+class RecursionExample{
+
+public static void main(String args[]){
+
+int result = sum(10);
+
+System.out.println(result);
+
+}
+
+public static int sum(int a ){
+
+if(a > 0){
+ 
+return a+sum(a-1);
+
+}else{
+
+return 0;
+
+}
+
+}
+
+}
+
+ 
---
+++ RuntimeDemo.java
@@ -0,0 +1,29 @@
+//package com.tutorialspoint;
+
+public class RuntimeDemo {
+
+   public static void main(String[] args) {
+
+      
+      System.out.println("Program starting...");
+
+            System.out.println("Program starting...");
+
+	 // Runtime run = Runtime.getRuntime();
+
+Runtime obj = Runtime.getRuntime();
+
+//obj.halt();
+
+      //Runtime.getRuntime().halt(0);
+
+//run.halt(0);
+
+//return halt(0);
+
+System.exit(0);
+
+      
+      System.out.println("Process is still running.");
+   }
+}
